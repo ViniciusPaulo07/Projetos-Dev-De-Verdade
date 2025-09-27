34a013afe1b51fc1c1b09a791c31391972bd1e28
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-09-27 20:25:03 -0300

    Comentarios para logica

--- javascript/Javascript-básico/Exercicios/Number-string.js
+++ javascript/Javascript-básico/Exercicios/Number-string.js
@@ -2,6 +2,7 @@ let numero1 = 10
 let texto = `${numero1}`//numero1 + ""(Concatenação)
 
 console.log(texto)
+//O typeof mostra o tipo da variável
 console.log(typeof texto)
 
 
@@ -15,6 +16,7 @@ console.log(typeof valor)
 let num = 42;
 let str = "123";
 
+//Converte número para string e string para número
 let numToStr = num.toString();
 let strToNum = Number(str);
 
