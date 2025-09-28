26d9dce4be0145298cd47d61242e5fc87d4f4e3e
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-09-27 22:53:46 -0300

    Aprendendo sobre metodos arrays

---
+++ javascript/Javascript-avançado/Metodos-arrays.js
@@ -0,0 +1,54 @@
+const numeros = [1, 2, 3, 4, 5]
+
+// forEach - Executa uma função para cada elemento do array
+numeros.forEach((elemento) => {
+    //console.log(elemento)
+})
+
+// map - Retorna um novo array, com a função aplicada em cada elemento do array original
+// numeros.map(elemento => elemento * 2)
+const arrayDobrado = numeros.map((elemento) => {
+    return elemento * 2
+})
+
+//console.log(arrayDobrado);
+
+// filter - Retorna um novo array, com os elementos que passaram pela condição
+const arrayFiltrado = numeros.filter((elemento) => {
+    return elemento % 2 === 0
+})
+
+//console.log(arrayFiltrado);
+
+// find - Retorna o primeiro elemento que passou pela condição
+const pesquisa = numeros.find((elemento) => {
+    return elemento == 5
+})
+
+//console.log(pesquisa);
+
+// some - Retorna true ou false, se algum elemento passou pela condição
+const some = numeros.some((elemento) => {
+    return elemento === 1
+})
+
+//console.log(some);
+
+// every - Retorna true ou false, se todos os elementos passaram pela condição
+const every = numeros.every((elemento) => {
+    return elemento === 10
+})
+
+//console.log(every);
+
+const produtos = [
+    {nome: 'Notebook', preco: 3000},
+    {nome: 'Teclado', preco: 100},
+    {nome: 'Mouse', preco: 50},
+    {nome: 'Monitor', preco: 800}
+]
+
+const buscarProduto = produtos.filter((produto) => {
+    return produto.preco < 50
+})
+
