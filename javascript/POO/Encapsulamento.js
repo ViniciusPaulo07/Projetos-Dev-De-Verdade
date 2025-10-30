a60b97db5591973dc9dbc9d99c37fec5f974d15b
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-10-30 09:42:48 -0300

    Continuação da aula de encapsulamento

--- javascript/POO/Encapsulamento.js
+++ javascript/POO/Encapsulamento.js
@@ -28,5 +28,22 @@ class Cofre {
         }
     }
 
-    sacar
+    sacar (senha, valor) {
+        if (this.#validarSenha(senha)){
+            if(valor > this.#saldo) {
+                console.log (`O valor a ser retirado é maior do que o saldo`)
+            } else {
+                this.#saldo -= valor
+                console.log (`${valor} foi retirado, saldo restante R$ ${this.#saldo}`)
+            }
+        } else {
+            console.log(`Senha inválida`)
+        }
+    }
 }
+
+const cofre = new Cofre('12345')
+cofre.depositar('12345', 100)
+cofre.verSaldo('12345')
+cofre.sacar('12345', 70)
+cofre.verSaldo('123457')
